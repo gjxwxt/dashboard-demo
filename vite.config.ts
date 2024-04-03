06c924bf5b78b7bc778357aacaead5dd1e12358e
gaojiaxiang <gaojiaxiang2018@gmail.com>
2024-04-03 15:56:31 +0800

    chore: 更改vite打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -58,5 +58,39 @@ export default defineConfig({
       }, // api代理路径
       '/mock': '' // mock代理路径,
     }
+  },
+  // 生产打包时采用rollup进行打包，
+  build: {
+    outDir: 'dist',
+    target: 'es2015', // 设置最终构建的浏览器兼容目标。
+    assetsDir: 'assets', // 指定生成静态资源的存放路径,相对于outDir
+    assetsInlineLimit: 4096, // 默认4096kb，小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。
+    sourcemap: false, // 默认是false，单独生成sourcemap文件
+    reportCompressedSize: false, // gzip 压缩大小报告
+    // minify: "esbuild", // 默认是esbuild，如果使用terser，需要安装terser npm i terser -D
+    // esbuild 打包更快，但是不能去除 console.log，terser打包慢，但能去除 console.log
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      treeshake: true,
+      output: {
+        // Static resource classification and packaging
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // 让每个插件都打包成独立的文件
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 });
